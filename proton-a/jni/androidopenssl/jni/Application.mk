0c4d2a8cc3028e19d937c8e8e403239f0fcb3348
jimmypc92 <t-jicamp@microsoft.com>
2014-06-22 04:13:55 -0700

    Added proton-a. this is the foundation for proton on Android. It contains necessary headers for Android build, the javabinding c wrapper, android openssl, android uuid, and an android example project.

---
+++ proton-a/jni/androidopenssl/jni/Application.mk
@@ -0,0 +1,4 @@
+#APP_ABI := armeabi,armeabi-v7a notice how you can comma separate archictectures
+APP_ABI := all
+APP_PROJECT_PATH := $(shell pwd)
+APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
---
+++ proton-a/jni/androiduuid/jni/Application.mk
@@ -0,0 +1,2 @@
+#APP_ABI := armeabi,armeabi-v7a		Notice how you can comma separate architectures.
+APP_ABI := all
