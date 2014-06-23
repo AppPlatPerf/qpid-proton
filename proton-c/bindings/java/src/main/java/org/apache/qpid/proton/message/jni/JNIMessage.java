b3c947ea2be7771c7f9b7a86bdd77c7e6bb55291
jimmypc92 <t-jicamp@microsoft.com>
2014-06-23 11:05:55 -0700

    Fixed issues that caused segfaults on Android. getprotobyname in posix/driver.c and convert in JNIMessage.java

--- proton-c/bindings/java/src/main/java/org/apache/qpid/proton/message/jni/JNIMessage.java
+++ proton-c/bindings/java/src/main/java/org/apache/qpid/proton/message/jni/JNIMessage.java
@@ -56,6 +56,7 @@ import org.apache.qpid.proton.jni.pn_bytes_t;
 import org.apache.qpid.proton.jni.pn_decimal128_t;
 import org.apache.qpid.proton.jni.pn_format_t;
 import org.apache.qpid.proton.jni.pn_type_t;
+import org.apache.qpid.proton.jni.pn_uuid_t;
 import org.apache.qpid.proton.message.Message;
 import org.apache.qpid.proton.message.MessageError;
 import org.apache.qpid.proton.message.MessageFormat;
@@ -1044,8 +1045,9 @@ public class JNIMessage implements Message
             }
             else if(pn_type_t.PN_UUID.equals(type))
             {
-                byte[] b = Proton.pn_bytes_to_array(value.getAs_bytes());
-                ByteBuffer buf = ByteBuffer.wrap(b);
+                pn_uuid_t uuidT = value.getAs_uuid();
+                byte[] uuidBytes = uuidT.getBytes();
+                ByteBuffer buf = ByteBuffer.wrap(uuidBytes);
                 return new UUID(buf.getLong(), buf.getLong());
             }
 
